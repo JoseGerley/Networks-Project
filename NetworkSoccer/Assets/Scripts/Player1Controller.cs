b2d7db04c9419ed7d6ee45c3a23ec4f092359d31
cristianm123 <crisfemoar2011@hotmail.com>
2019-09-22 11:03:20 -0500

    Unity

---
+++ NetworkSoccer/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1Controller : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    //public Text countText;
+    //public Text winText;
+
+    public float speed;
+    //private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        //count = 0;
+        //winText.text = "";
+        //SetCountText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+
+    /*
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PickUp"))
+        {
+            collision.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "YOU WIN!!";
+        }
+    }
+    */
+}
